16c62152ec91c371b542ed861694295445b168c9
leopardo <leonardoandrespardo@gmail.com>
2023-08-31 09:55:16 -0300

    fix horario

--- src/crons/MeliCron.ts
+++ src/crons/MeliCron.ts
@@ -2,9 +2,8 @@ import { MeliTask } from "../tasks/MeliTask";
 
 const cron = require('node-cron');
 
-cron.schedule('0 10 * * *', () => {
+cron.schedule('0 13 * * *', () => {
     new MeliTask().run();
 }, {
     scheduled: true,
-    timezone: "America/Buenos_Aires"
 });
