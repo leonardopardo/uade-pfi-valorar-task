99dd625ed1753ecc7915807ebca06d8ccab92c1c
leopardo <leonardoandrespardo@gmail.com>
2023-08-31 10:06:28 -0300

    fix date 2

--- src/crons/MeliCron.ts
+++ src/crons/MeliCron.ts
@@ -2,7 +2,7 @@ import { MeliTask } from "../tasks/MeliTask";
 
 const cron = require('node-cron');
 
-cron.schedule('0 13 * * *', () => {
+cron.schedule('0 10 13 * *', () => {
     new MeliTask().run();
 }, {
     scheduled: true,
